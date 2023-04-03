da1698ba2cc468433cc2fa946b9205cc58577def
sony-vedom <minerva.lankmiller@gmail.com>
2023-04-03 18:39:12 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-const response = fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1").then((response) =>
-    response.formData().get()
-)
+import jQuery from "jquery"
+
+window.$ = window.jQuery = jQuery;
+
+const api = async () => {
+    let promise = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1")
+    let response = await promise.json();
+
+    for (let i = 0; i < response; i++) {
+        cardComponent().appendTo($("#root-cards"))
+    }
+
+}
---
+++ src/webpack.config.js
@@ -0,0 +1,76 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const postcssPresetEnv = require('postcss-preset-env');
+
+let mode = "development"
+if (process.env.NODE_ENV === "production") {
+    mode = "production"
+}
+module.exports = {
+    mode: mode,
+    // entry: {
+    //     scripts: './src/index.js',
+    // },
+    output: {
+        assetModuleFilename: "assets/[hash][ext][query]",
+        filename: "[name].[contenthash].js",
+        clean: true,
+    },
+    devtool: "source-map",
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].[contenthash].css"
+        }),
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            chunks: ['main'],
+            scriptLoading: 'defer',
+            inject: "body",
+        })],
+    module: {
+        rules: [
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    postcssPresetEnv()
+                                ]
+                            }
+                        }
+                    },
+                    "sass-loader"
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'assets/resource',
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader"
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/,
+                type: 'asset/resource'
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"]
+                    }
+                }
+            },
+
+        ]
+    }
+
+}
