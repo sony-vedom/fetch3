f9a65b5e133c9f65e3847410503046a2f5cc6f72
sony-vedom <minerva.lankmiller@gmail.com>
2023-04-03 18:51:26 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-import jQuery from "jquery"
+// import jQuery from "jquery"
 
-window.$ = window.jQuery = jQuery;
+// window.$ = window.jQuery = jQuery;
 
 const api = async () => {
     let promise = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1")
-    let response = await promise.json();
-
-    for (let i = 0; i < response; i++) {
-        cardComponent().appendTo($("#root-cards"))
-    }
-
+    return await promise.json();
 }
+
+console.log(api())
